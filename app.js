73bf4726c1ddc24128f192135d003be556a0e605
aimobina1 <aiworkgoal@gmail.com>
2022-01-20 21:26:17 +0330

    filtering todos completed by adding class to filtered class and convert them to lower case to search

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add')
 const list = document.querySelector('.todos')
+const search = document.querySelector('.search input')
 //add todos
 const generateTemplate = todo =>{
     const html = `
@@ -24,4 +25,21 @@ list.addEventListener('click', e=>{
     if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
     } 
-})
+});
+
+//filtering todos
+
+const filterToDos=(term)=>{
+    Array.from(list.children)
+    .filter((todo)=>!todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter((todo)=>todo.textContent.toLowerCase().includes(term))
+         .forEach((todo) => todo.classList.remove('filtered'))
+};
+
+ search.addEventListener('keyup',()=>{
+    const term = search.value.trim().toLowerCase();
+    filterToDos(term);
+ });
